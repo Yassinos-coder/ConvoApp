b82df0cee975f9691b60bfd4a2fe4096abf8cb93
Yassinos-Coder <37705952+Yassinos-coder@users.noreply.github.com>
2023-09-06 15:39:05 +0100

    Done Some Changes

---
+++ backend/core.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const fileUpload = require('express-fileupload')
+
+require('dotenv').config()
+
+const app = express()
+
+app.use(express.static('uploads'))
+app.use(express.json())
+app.use(cors())
+app.use(fileUpload())
+app.listen(process.env.BACK_END, () => console.info(`Back-End Convo Started Succesfully on port => ${process.env.BACK_END}!`))
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.info('Database connection granted');
+    db_connection_success = true
+}).catch(error => {
+    db_connection_success = false
+    console.warn('Database connection error:', error.message);
+});
