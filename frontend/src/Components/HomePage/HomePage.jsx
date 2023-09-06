56e37fcf7082a58cc2e9fb6c540a21f8a1908e35
Yassinos-Coder <37705952+Yassinos-coder@users.noreply.github.com>
2023-09-06 17:10:57 +0100

    Added Footer, Text, Images...

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import "./HomePage.css";
 import React from "react";
 import appLogo from "../../Assets/Images/appLogo.png";
+import appLogoFull from "../../Assets/Images/appLogoFull.png";
 
 const HomePage = () => {
   return (
@@ -9,7 +10,9 @@ const HomePage = () => {
         <div className="headerContent">
           <div className="title">
             <img className="appLogoHeader" src={appLogo} alt="" />
-            <p className="pHeader">Convo Chat</p>
+            <p className="pHeader">
+              Convo <span className="spanChat">CHAT</span>
+            </p>
           </div>
           <div className="actions">
             {localStorage.getItem("user_status") === "connected" ? (
@@ -25,6 +28,25 @@ const HomePage = () => {
           </div>
         </div>
       </div>
+      <div className="main">
+        <img className="appLogoHomePage" src={appLogoFull} alt="" />
+        <div className="text">
+          <h2>1- What is Convo ?</h2>
+          <p>
+            - Welcome to convoChat, where safety, security, and seamless
+             performance come together in one powerful chat app.
+          </p>
+          <p>
+            - Built with the cutting-edge MERN stack, convoChat ensures your
+            conversations
+            remain private, your data stays secure, and your chats flow
+            effortlessly.
+          </p>
+        </div>
+      </div>
+      <footer>
+        <h1>Developed with ❤️ by <a href="https://github.com/Yassinos-coder">Yassinos-Coder</a></h1>
+      </footer>
     </div>
   );
 };
